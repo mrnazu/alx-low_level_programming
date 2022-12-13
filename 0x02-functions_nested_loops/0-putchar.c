066fb2567201ca973dedea495b8f324bdc7d51b5
mrnazu <samuelamsalu1998@gmail.com>
2022-12-13 07:52:47 -0800

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - print _putchare
+ * @void: no argument
+ * Return: meas sucess
+ */
+int main(void)
+{
+	char hacker[] = "_putchar";
+
+	int n;
+
+	for (c = 0; c < 8; n++)
+	{
+		_putchar(hacker[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
