6512d3def945f8a544f168144cae353b65132771
mrnazu <samuelamsalu1998@gmail.com>
2022-12-15 13:18:13 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ *
+ * @n: integer
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
